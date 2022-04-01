d1c1b9c33751d580c9daaae6802b291e5257a2d1
Jumanazar <jsr1611@outlook.com>
2022-04-01 16:33:37 +0900

    added eureka discovery server at port 8761

--- demo2/src/main/java/com/example/demo2/controller/TestController.java
+++ demo2/src/main/java/com/example/demo2/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping
-    public ResponseEntity hello(){
+    public ResponseEntity hello() {
         return ResponseEntity.ok("Hello from Demo2");
     }
 
